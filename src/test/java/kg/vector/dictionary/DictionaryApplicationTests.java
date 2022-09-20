57e1b136b472c345c15b33b55d78488dace133ea
Alfit20 <alfit.bashirov@gmail.com>
2022-09-20 19:16:22 +0600

    Initial commit

---
+++ src/test/java/kg/vector/dictionary/DictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.vector.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
